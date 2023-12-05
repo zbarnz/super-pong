fdb550d12d9d4f39bdb74ce1f85495b35491ab09
David Oligney <davidoligney@gmail.com>
2023-12-05 13:45:13 -0700

    added angles to ball bouncing off wall/paddle

--- frontend/src/components/Pong.svelte
+++ frontend/src/components/Pong.svelte
@@ -53,20 +53,68 @@
         ball.y - ball.radius < 0 ||
         ball.y + ball.radius > canvasDimensions.height
       ) {
-        ball.speedY = -ball.speedY;
+        
+        // Calculate the angle using atan2
+        const angle = Math.atan2(ball.speedY, ball.speedX);
+
+        // The resulting angle is between -π and π
+        // Convert it to a positive angle between 0 and 2π for consistency
+        const wallBounceAngle = angle < 0 ? 2 * Math.PI + angle : angle;
+
+        const speedMagnitude = Math.sqrt(ball.speedX ** 2 + ball.speedY ** 2);
+
+        // Calculate the new x and y components based on the angle
+        const newSpeedX = speedMagnitude * Math.cos(wallBounceAngle);
+        const newSpeedY = speedMagnitude * Math.sin(wallBounceAngle);
+
+        // Update the ball's speed
+        ball.speedX = newSpeedX;
+        ball.speedY = -newSpeedY;
       }
   
       // Bounce off the paddles
       if (
         (ball.x - ball.radius < paddleWidth &&
           ball.y > leftPaddleY &&
-          ball.y < leftPaddleY + paddleHeight) ||
+          ball.y < leftPaddleY + paddleHeight))
+          {
+        // Adjust the ball's angle based on where it hits the paddle
+        const deltaY = ball.y - (leftPaddleY + paddleHeight / 2);
+        const normalizedDeltaY = deltaY / (paddleHeight / 2);
+
+        // Set the new angle (modify as needed)
+        const maxBounceAngle = Math.PI / 4; // Maximum bounce angle in radians
+        const bounceAngle = normalizedDeltaY * maxBounceAngle;
+
+        // Update the ball's speed and direction
+        const speed = Math.sqrt(ball.speedX ** 2 + ball.speedY ** 2);
+        const newSpeedX = speed * Math.cos(bounceAngle);
+        const newSpeedY = speed * Math.sin(bounceAngle);
+
+        ball.speedX = newSpeedX;
+        ball.speedY = newSpeedY;
+      } 
+      else if(
         (ball.x + ball.radius > canvasDimensions.width - paddleWidth &&
           ball.y > rightPaddleY &&
           ball.y < rightPaddleY + paddleHeight)
-      ) {
-        ball.speedX = -ball.speedX;
-      }
+      ){
+        // Adjust the ball's angle based on where it hits the paddle
+        const deltaY = ball.y - (rightPaddleY + paddleHeight / 2);
+        const normalizedDeltaY = deltaY / (paddleHeight / 2);
+
+        // Set the new angle (modify as needed)
+        const maxBounceAngle = Math.PI / 4; // Maximum bounce angle in radians
+        const bounceAngle = normalizedDeltaY * maxBounceAngle;
+
+        // Update the ball's speed and direction
+        const speed = Math.sqrt(ball.speedX ** 2 + ball.speedY ** 2);
+        const newSpeedX = speed * Math.cos(bounceAngle);
+        const newSpeedY = speed * Math.sin(bounceAngle);
+
+        ball.speedX = -newSpeedX;
+        ball.speedY = newSpeedY;
+      } 
   
       // Reset ball if it goes out of bounds
       if (ball.x - ball.radius < 0 || ball.x + ball.radius > canvasDimensions.width) {
